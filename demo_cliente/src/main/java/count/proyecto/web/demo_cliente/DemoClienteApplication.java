157d1c21140fae1ad229306ca100d8c11ba46dcb
JhonArquitectura <159597856+JhonArquitectura@users.noreply.github.com>
2024-06-12 20:17:36 -0500

    Add files via upload

---
+++ demo_cliente/src/main/java/count/proyecto/web/demo_cliente/DemoClienteApplication.java
@@ -0,0 +1,13 @@
+package count.proyecto.web.demo_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClienteApplication.class, args);
+	}
+
+}
---
+++ demo_cliente/src/test/java/count/proyecto/web/demo_cliente/DemoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package count.proyecto.web.demo_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
