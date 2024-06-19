96dc9763b4a2c8f9a78f7ba2e15f4542349fbb90
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-06-19 09:59:30 -0500

    Create Generos.java
    Se creó el modelo de los Generos

---
+++ demo_cliente/src/main/java/count/proyecto/web/demo_cliente/Models/Generos.java
@@ -0,0 +1,27 @@
+package count.proyecto.web.demo_cliente.Models;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "generos")
+public class Usuarios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "genero")
+    private String genero;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+}
