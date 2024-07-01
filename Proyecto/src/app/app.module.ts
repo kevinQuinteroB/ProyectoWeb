58a6ecf14b1195897fe591712057a614b0720604
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-07-01 16:05:00 -0500

    carpeta juego
    Se creo la carpeta juego y se le hicieron unos cambios de diseño a login

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { JuegoComponent } from './juego/juego.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    JuegoComponent
   ],
   imports: [
     BrowserModule,
