772f31d5e3bbd2d1465c574c5aaa8d812844c0a3
Kevin Quintero <kevinstequintero@gmail.com>
2024-07-03 14:46:36 -0500

    Creó la direccion para el perfil del jugador

--- Proyecto/src/app/app-routing.module.ts
+++ Proyecto/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { JuegoComponent } from './juego/juego.component'; 
+import { PerfiljugadorComponent } from './perfiljugador/perfiljugador.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'/login', pathMatch:'full'},
   { path: 'login', component: LoginComponent },
   { path: 'juego', component: JuegoComponent },
+  { path: 'perfil', component: PerfiljugadorComponent},
 ];
 
 @NgModule({
