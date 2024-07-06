0e69cf6b04fef2b405901f9682e1034d1fbec53c
JhonAlmanzar <159461041+JhonAlmanzar@users.noreply.github.com>
2024-07-06 13:27:06 -0500

    Arreglos

--- demo_cliente/src/main/java/count/proyecto/web/demo_cliente/Controller/ComentarioController.java
+++ demo_cliente/src/main/java/count/proyecto/web/demo_cliente/Controller/ComentarioController.java
@@ -2,6 +2,7 @@ package count.proyecto.web.demo_cliente.Controller;
 
 import count.proyecto.web.demo_cliente.Models.Comentario;
 import count.proyecto.web.demo_cliente.Services.ComentarioService;
+import count.proyecto.web.demo_cliente.Services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,23 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import java.util.List;
 
 @Controller
 public class ComentarioController {
     @Autowired
     private ComentarioService comentarioService;
 
-    @GetMapping("/juego/{juegoId}/comentarios")
-    public ResponseEntity<List<Comentario>> retunComentariosPorJuego(@PathVariable Long juegoId) {
-        List<Comentario> comentarios = comentarioService.findByJuegoId(juegoId);
-        return ResponseEntity.ok(comentarios);
+    @GetMapping("/login/{id_juego}")
+    public ResponseEntity<Comentario> retunComentario(@PathVariable Long id_juego) {
+        return ResponseEntity.ok(comentarioService.findComentarioByid_juego(id_juego));
     }
 
-    @PostMapping("/juego/comentario")
-    public ResponseEntity<Comentario> registrarComentario(@RequestBody Comentario comentario) {
+    @PostMapping("/login")
+    public ResponseEntity<Comentario>registrarComentario(@RequestBody Comentario comentario){
         Comentario newComentario = comentarioService.saveComentario(comentario);
         return ResponseEntity.ok(newComentario);
     }
 }
-
--- demo_cliente/src/main/java/count/proyecto/web/demo_cliente/Repository/ComentarioRepository.java
+++ demo_cliente/src/main/java/count/proyecto/web/demo_cliente/Repository/ComentarioRepository.java
@@ -1,15 +1,13 @@
 package count.proyecto.web.demo_cliente.Repository;
 
 import count.proyecto.web.demo_cliente.Models.Comentario;
-import count.proyecto.web.demo_cliente.Models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
-
 
 @Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
-    @Query(value = "SELECT * FROM comentario WHERE juego_id = :juegoId", nativeQuery = true)
-    List<Comentario> findByJuegoId(Long juegoId);
+    @Query(value = "SELECT * FROM Comentario WHERE id_juego = :id_juego" , nativeQuery = true)
+    Comentario findByid_juego(Long id_juego);
 }
