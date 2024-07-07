ffb597f0997d57050b4eb028574f39842f549e9f
Kevin Quintero <kevinstequintero@gmail.com>
2024-07-07 11:52:38 -0500

    se arregló la barra de jhon

--- Proyecto/src/app/home/home.component.ts
+++ Proyecto/src/app/home/home.component.ts
@@ -8,17 +8,6 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
 }
-document.addEventListener('DOMContentLoaded', () => {
-  const menuBtn = document.querySelector('.menu-btn') as HTMLButtonElement;
-  const header = document.querySelector('.header') as HTMLElement;
-
-  if (menuBtn && header) {
-    menuBtn.addEventListener('click', () => {
-      header.classList.toggle('dropdown-active');
-    });
-  }
-});
-
 
 document.addEventListener('DOMContentLoaded', function() {
   const barraSticky = document.getElementById('barraSticky');
